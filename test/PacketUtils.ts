9617f1ce658da80fa84887c6f46ef54b95ce54c0
Srar <a1020938070@gmail.com>
2018-03-12 02:47:30 +0800

    移除未被使用的方法

--- test/PacketUtils.ts
+++ test/PacketUtils.ts
@@ -4,8 +4,6 @@ import {
     ArpPacket,
     IpPacket
 } from "./PacketsStruct"
-import ArpPacketFormatter from "./formatters/ArpPacketFormatter"
-
 
 export default {
 
@@ -36,29 +34,6 @@ export default {
 
     isIGMP: function (bufs): boolean {
         return bufs[23] === 0x02;
-    },
-
-    inetAddr: function (ip) {
-        var nip = ip.split(".").map(function (item) {
-            return parseInt(item);
-        })
-        var bufs = Buffer.from(nip);
-        return bufs.readUInt32LE(0);
-    },
-
-    inetNtoa: function (number) {
-        var bufs = new Buffer(4);
-        bufs.writeUInt32BE(number, 0);
-        return `${bufs[3].toString(10)}.${bufs[2].toString(10)}.${bufs[1].toString(10)}.${bufs[0].toString(10)}`;
-    },
-
-    formatIpPacket: function(bufs: Buffer): IpPacket {
-        var basePacket: BasePacket = this.formatBasePacket(bufs);
-        var packet = {
-            
-        }
-        packet = Object.assign(basePacket, packet);
-        return <IpPacket>packet;
     },
 
     stringToIpAddress: function (ip): Buffer {
