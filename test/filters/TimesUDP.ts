c7ad71f54cc1af146a2615a4387baf5fa867fec2
Srar <a1020938070@gmail.com>
2018-05-22 05:46:58 +0800

    修正解析IP错误问题

--- test/filters/TimesUDP.ts
+++ test/filters/TimesUDP.ts
@@ -16,16 +16,16 @@ function registerXTUdp() {
     console.log("Default gateway of adapter:", Config.get("DefaultGateway"));
 
     /* 注册Pcap */
-    var cap = new Cap();
-    var device = Cap.findDevice(Config.get("DefaultIp"));
-    var filter = `udp and dst host ${Config.get("ShadowsocksUdpHost")}`;
-    var bufSize = 10 * 1024 * 1024;
-    var buffer = Buffer.alloc(65535);
-    var linkType = cap.open(device, filter, bufSize, buffer);
+    const cap = new Cap();
+    const device = Cap.findDevice(Config.get("DefaultIp"));
+    const filter = `udp and dst port ${Config.get("ShadowsocksUdpPort")} and dst host ${Config.get("ShadowsocksUdpHost")}`;
+    const bufSize = 10 * 1024 * 1024;
+    const buffer = Buffer.alloc(65535);
+    const linkType = cap.open(device, filter, bufSize, buffer);
     cap.setMinBytes && cap.setMinBytes(0);
 
     /* 注册RawScoket */
-    var rawsocket = raw.createSocket({
+    const rawsocket = raw.createSocket({
         protocol: raw.Protocol.UDP
     });
     rawsocket.setOption(raw.SocketLevel.IPPROTO_IP, raw.SocketOption.IP_HDRINCL, new Buffer([0x00, 0x00, 0x00, 0x01]), 4);
@@ -36,9 +36,9 @@ function registerXTUdp() {
         if (nbytes < 34) return;
         if (buffer[22] == SPECIAL_TTL) return;
         buffer[22] = SPECIAL_TTL;
-        var sendingBuffer = buffer.slice(14, nbytes);
-        var targetIpAddress: string = PacketUtils.ipToString(sendingBuffer.slice(16, 21));
-        for (var i = 1; i < XTUdp; i++) {
+        const sendingBuffer = buffer.slice(14, nbytes);
+        const targetIpAddress: string = PacketUtils.ipToString(sendingBuffer.slice(16, 20));
+        for (let i = 1; i < XTUdp; i++) {
             rawsocket.send(sendingBuffer, 0, sendingBuffer.length, targetIpAddress, function (error, bytes) {
                 if (error) {
                     console.error(error);
